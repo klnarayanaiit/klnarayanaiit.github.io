652d895ea4a398583da003db61a2e74698a20584
Lux@NANI <newnarayanabv2014@gmail.com>
2022-09-25 15:15:47 +0530

    Updates

---
+++ precache-manifest.3ac820ac53cb9087585fb8b57dfa215f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "968892fd28e94181f1ca",
+    "url": "/klnarayanaiit/klnarayanaiit.github.io.git/static/css/main.23106250.chunk.css"
+  },
+  {
+    "revision": "968892fd28e94181f1ca",
+    "url": "/klnarayanaiit/klnarayanaiit.github.io.git/static/js/main.968892fd.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/klnarayanaiit/klnarayanaiit.github.io.git/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "a45e404491eda03f0d0e",
+    "url": "/klnarayanaiit/klnarayanaiit.github.io.git/static/js/2.a45e4044.chunk.js"
+  },
+  {
+    "revision": "b1b147a24039e9fe59b4",
+    "url": "/klnarayanaiit/klnarayanaiit.github.io.git/static/js/runtime~main.b1b147a2.js"
+  },
+  {
+    "revision": "e13c68574c5613c23aed40843beee77c",
+    "url": "/klnarayanaiit/klnarayanaiit.github.io.git/static/media/Gaming.e13c6857.jpg"
+  },
+  {
+    "revision": "4dd5ac02df5991601580b41527112a37",
+    "url": "/klnarayanaiit/klnarayanaiit.github.io.git/index.html"
+  }
+];
